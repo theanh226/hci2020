e7cc9e594fb61eda32333e1054ce96da7e9e8493
Trinh Ngoc Anh <ngocanhtrinh94@gmail.com>
2020-12-11 21:06:03 +0100

    login-page

--- pages/login/index.js
+++ pages/login/index.js
@@ -1,6 +1,5 @@
 import React from "react";
-import { Input } from 'antd';
-import { Button } from "antd";
+import { Input,Button } from "antd";
 import Layout from "../../layouts/index";
 import styles from "./Login.module.css";
 
@@ -9,18 +8,12 @@ const LoginPage = () => {
     <Layout>
       <form>
         <div className={styles.login}>
-          <div className={styles.input}>
-            <Input placeholder="Username" />
-          </div>
-          <div className={styles.input}>
-            <Input.Password placeholder="Password" />
-          </div>
+          <Input placeholder="Username" className={styles.input}/>
+          <Input.Password placeholder="Password" className={styles.input}/>
           <div className={styles.register}>
             <p>Don't have an account? <a href="#" >Register</a></p>
           </div>
-          <div className={styles.signin}>
-            <Button type="primary" block>Sign in</Button>
-          </div>
+          <Button type="primary" block>Sign in</Button>
         </div>
       </form>
     </Layout>
