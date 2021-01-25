ff4a6ade926a40ee293ff536a891dfcc29cb77cf
Anh Nguyen <nu2tmt4@gmail.com>
2021-01-25 14:55:55 +0100

    Improve personal-info

--- pages/personal-info/index.js
+++ pages/personal-info/index.js
@@ -1,24 +1,25 @@
-import React from 'react';
-import Sider from '../../components/sider/Sider';
-import LayoutMain from '../../layouts/index';
-import { Row, Col, Layout, Button } from 'antd';
-import PersonalInfoEdit from '../../components/personal-info-edit/personal-info-edit';
+import React from "react";
+import Sider from "../../components/sider/Sider";
+import LayoutMain from "../../layouts/index";
+import { Row, Col, Layout, Avatar } from "antd";
+import { UserOutlined } from "@ant-design/icons";
+import PersonalInfoEdit from "../../components/personal-info-edit/personal-info-edit";
 
 const { Content } = Layout;
 
 const PersonalInfo = () => {
   return (
     <LayoutMain>
-      <Layout style={{ background: 'unset' }}>
+      <Layout style={{ background: "unset" }}>
         <Sider highlight="3" />
-        <Layout className="site-layout" style={{ background: 'unset' }}>
-          <Content style={{ padding: '4em 17em' }}>
+        <Layout className="site-layout" style={{ background: "unset" }}>
+          <Content style={{ padding: "4em 17em" }}>
             <div
               className="site-layout-background"
               style={{
                 padding: 24,
-                textAlign: 'center',
-                background: '#f0f2f5',
+                textAlign: "center",
+                background: "#f0f2f5",
               }}
             >
               <h2>Personal Info</h2>
@@ -26,32 +27,28 @@ const PersonalInfo = () => {
                 <Col span={16}>
                   <Row
                     style={{
-                      textAlign: 'left',
-                      lineHeight: '3',
-                      fontSize: '1.2em',
+                      textAlign: "left",
+                      lineHeight: "3",
+                      fontSize: "1.2em",
                     }}
                   >
                     {/* Name */}
                     <Col span={8}>Name:</Col>
-                    <Col span={16}>Max Musteruserman</Col>
+                    <Col span={16}>SampleUser</Col>
                     {/* Mail */}
                     <Col span={8}>Mail:</Col>
-                    <Col span={16}>mustermail@mmm</Col>
+                    <Col span={16}>sample_user_mail@gmail.com</Col>
                     {/* Interest */}
                     <Col span={8}>Interest:</Col>
-                    <Col span={16}>Programming, Math</Col>
+                    <Col span={16}>None</Col>
                   </Row>
                 </Col>
                 <Col span={8}>
-                  <img
-                    src="https://a2.tvspielfilm.de/imedia/2748/10132748,h9mG+u3sBHexzIGgTqt6rWCk3asDJWPD6CWmzekVslQIkikfIsktlRkjOerP_1olW_iOZWnEXchssqyRkZG7Wg==.jpg"
-                    width="200"
-                    height="160"
-                  ></img>
+                  <Avatar shape="square" size={144} icon={<UserOutlined />} />
                 </Col>
               </Row>
             </div>
-            <div style={{ textAlign: 'Right', marginTop: '1em' }}>
+            <div style={{ textAlign: "Right", marginTop: "1em" }}>
               <PersonalInfoEdit />
             </div>
           </Content>
