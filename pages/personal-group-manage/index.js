02ae06eedb94d1a535c251209500a775068a05e6
Anh Nguyen <nu2tmt4@gmail.com>
2021-01-25 14:51:02 +0100

    improve inbox

--- pages/personal-group-manage/index.js
+++ pages/personal-group-manage/index.js
@@ -8,8 +8,6 @@ import { SearchOutlined } from "@ant-design/icons";
 
 // for hc part
 import data from "../../components/data/group-manage.json";
-import Link from "next/link";
-import { toast } from "react-toastify";
 
 const { Content } = Layout;
 
--- pages/personal-inbox/index.js
+++ pages/personal-inbox/index.js
@@ -1,12 +1,128 @@
 import React from "react";
-import Sider from "../../components/sider/Sider";
 import LayoutMain from "../../layouts/index";
-import Mail from "../../components/mail/Mail";
-import { Row, Col, Layout, Button } from "antd";
+import Sider from "../../components/sider/Sider";
+import { Popconfirm, Layout, Button, Table, Input, Space } from "antd";
+
+import { UserAddOutlined, DeleteOutlined, SearchOutlined } from "@ant-design/icons";
+
+// for hc part
+import data from "../../components/data/inbox.json";
 
 const { Content } = Layout;
 
 const PersonalInbox = () => {
+  const [searchedColumn, setSearchColumn] = "";
+
+  const getColumnSearchProps = (dataIndex) => ({
+    filterDropdown: ({
+      setSelectedKeys,
+      selectedKeys,
+      confirm,
+      clearFilters,
+    }) => (
+      <div style={{ padding: 8 }}>
+        <Input
+          placeholder={`Search ${dataIndex}`}
+          value={selectedKeys[0]}
+          onChange={(e) =>
+            setSelectedKeys(e.target.value ? [e.target.value] : [])
+          }
+          onPressEnter={() => handleSearch(selectedKeys, confirm, dataIndex)}
+          style={{ width: 188, marginBottom: 8, display: "block" }}
+        />
+        <Space>
+          <Button
+            type="primary"
+            onClick={() => handleSearch(selectedKeys, confirm, dataIndex)}
+            icon={<SearchOutlined />}
+            size="small"
+            style={{ width: 90 }}
+          >
+            Search
+          </Button>
+          <Button
+            onClick={() => handleReset(clearFilters)}
+            size="small"
+            style={{ width: 90 }}
+          >
+            Reset
+          </Button>
+        </Space>
+      </div>
+    ),
+    filterIcon: (filtered) => (
+      <SearchOutlined style={{ color: filtered ? "#1890ff" : undefined }} />
+    ),
+    onFilter: (value, record) =>
+      record[dataIndex]
+        ? record[dataIndex]
+            .toString()
+            .toLowerCase()
+            .includes(value.toLowerCase())
+        : "",
+    onFilterDropdownVisibleChange: (visible) => {
+      if (visible) {
+      }
+    },
+    render: (text) => (searchedColumn === dataIndex ? <></> : text),
+  });
+
+  const handleSearch = (selectedKeys, confirm, dataIndex) => {
+    confirm();
+  };
+
+  const handleReset = (clearFilters) => {
+    clearFilters();
+  };
+
+  const columns = [
+    {
+      title: "Sender",
+      dataIndex: "sender",
+      key: "sender",
+      width: "20%",
+      ...getColumnSearchProps("sender"),
+    },
+    {
+      title: "Content",
+      dataIndex: "content",
+      key: "content",
+      width: "30%",
+      ...getColumnSearchProps("content"),
+    },
+    {
+      title: "Time",
+      dataIndex: "time",
+      key: "time",
+      ...getColumnSearchProps("time"),
+    },
+    {
+      title: "Option",
+      dataIndex: "option",
+      key: "option",
+      render: (option) => (
+        <>
+          {option == "Accept" ? (
+            <Popconfirm
+              title="Are you sure?"
+              okText="Yes"
+              cancelText="No"
+            >
+              <Button type="primary" ><UserAddOutlined /></Button>
+            </Popconfirm>
+          ) : (
+            <Popconfirm
+              title="Do you want delete this mail?"
+              okText="Yes"
+              cancelText="No"
+            >
+              <Button type="danger"><DeleteOutlined /></Button>
+            </Popconfirm>
+          )}
+        </>
+      ),
+    },
+  ];
   return (
     <LayoutMain>
       <Layout style={{ background: "unset" }}>
@@ -16,41 +132,29 @@ const PersonalInbox = () => {
             <div
               className="site-layout-background"
               style={{
-                padding: "12px 24px 24px",
+                padding: 24,
                 textAlign: "center",
                 background: "#f0f2f5",
               }}
             >
               <h2>Inbox</h2>
-              <Row
-                style={{
-                  background: "white",
-                  fontWeight: "bold",
-                  textAlign: "left",
-                  paddingLeft: "0.5em",
+              <p></p>
+              {/* FILTER */}
+              <Table
+                columns={columns}
+                expandable={{
+                  expandedRowRender: (record) => (
+                    <p style={{ margin: 0 }}>{record.detail}</p>
+                  ),
+                  rowExpandable: (record) => record.detail !== "",
                 }}
-              >
-                <Col span={5}>Name</Col>
-                <Col span={7}>Content</Col>
-                <Col span={5}>Date</Col>
-                <Col span={7}>Option</Col>
-              </Row>
-              <Mail />
-              <Row
-                style={{
-                  background: "white",
-                  marginTop: "0.25em",
-                  textAlign: "left",
-                  paddingLeft: "0.5em",
+                pagination={{
+                  defaultPageSize: 7,
+                  showSizeChanger: true,
+                  pageSizeOptions: ["7", "14", "21"],
                 }}
-              >
-                <Col span={5}>Leyman Fey</Col>
-                <Col span={7}>You have been accepted to Let's learn math ...</Col>
-                <Col span={5}>1:00 AM 17/12/2020</Col>
-                <Col span={7}>
-                  <Button type="danger">Delete</Button>
-                </Col>
-              </Row>
+                dataSource={data}
+              />
             </div>
           </Content>
         </Layout>
@@ -58,4 +162,5 @@ const PersonalInbox = () => {
     </LayoutMain>
   );
 };
+
 export default PersonalInbox;
